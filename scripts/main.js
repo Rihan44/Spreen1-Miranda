b94af1e04132b6c630598af3675a2dd518b9df0c
Rihan44 <asmuela_bustamante@hotmail.es>
2023-09-18 13:34:24 +0200

    swippers rooms details and offers added

--- scripts/main.js
+++ scripts/main.js
@@ -23,5 +23,140 @@ burguer_icon.addEventListener('click', () => {
 });
 
 
+/* SWIPER HOME */
+
+const swiperRooms = new Swiper('#swipper-rooms', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+
+    navigation: {
+        nextEl: '#swiper-button-next-rooms',
+        prevEl: '#swiper-button-prev-rooms',
+    }
+
+});
 
 
+const swiperFacilites = new Swiper('#swiper-facilities', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true
+    }
+
+});
+
+let swiperMenu = new Swiper(".swiper-menu", {
+    cssMode: true,
+
+    navigation: {
+        nextEl: ".swiper-menu-buttons__next",
+        prevEl: ".swiper-menu-buttons__prev",
+    },
+});
+
+const swiperImagesFood = new Swiper('.swiper-images-food-slider', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+    cssMode: true,
+
+    pagination: {
+        el: ".swiper-pagination-images-food",
+        type: "bullets",
+        clickable: true
+    }
+
+});
+
+/* SWIPER ABOUT US */
+
+const swiperFacilitesAboutUs = new Swiper('#swiper-facilities-about-us', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true
+    }
+
+});
+
+const swiperCounter = new Swiper('#swiper-counter', {
+    direction: 'horizontal',
+
+    cssMode: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true
+    }
+
+});
+
+/* ROOMS */
+const swiper = new Swiper('#swipper-rooms-roomspage', {
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+
+
+/* SWIPER ROOMS DETAILS */
+const swiperRoomsDetails = new Swiper('#swipper-rooms-details', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+
+    navigation: {
+        nextEl: '#swiper-button-next-rooms-details',
+        prevEl: '#swiper-button-prev-rooms-details',
+    }
+
+});
+
+/* OFFERS */
+const swiperRoomsOffers = new Swiper('#swipper-rooms-offers', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+
+    navigation: {
+        nextEl: '#swiper-button-next-rooms-offers',
+        prevEl: '#swiper-button-prev-rooms-offers',
+    }
+
+});
