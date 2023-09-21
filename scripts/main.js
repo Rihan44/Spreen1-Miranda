4eca3687d545f25ac84ab1f19c452508bfd2f91f
Rihan44 <asmuela_bustamante@hotmail.es>
2023-09-21 22:15:37 +0200

    swiper-rooms-offers issues fixed

--- scripts/main.js
+++ scripts/main.js
@@ -247,28 +247,46 @@ if(swiper_rooms_details != null){
     initSwiperRoomsDetails();
 }
 
-
 /* OFFERS */
-const swiperRoomsOffers = new Swiper('#swipper-rooms-offers', {
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 3000
-    },
 
-    navigation: {
-        nextEl: '#swiper-button-next-rooms-offers',
-        prevEl: '#swiper-button-prev-rooms-offers',
+let swipper_rooms_offers = document.querySelector('#swipper-rooms-offers');
+let swiper_wrapper_rooms_offers = document.querySelector('.swiper-wrapper__rooms-offers');
+
+function initSwiperRoomsOffers() {
+    if (window.matchMedia("(max-width: 1000px").matches) {
+        swipper_rooms_offers.classList.add('swiper');
+        let swiperRoomsOffers = new Swiper('#swipper-rooms-offers', {
+            direction: 'horizontal',
+            loop: true,
+            autoplay: {
+                delay: 3000
+            },
+        
+            navigation: {
+                nextEl: '#swiper-button-next-rooms-offers',
+                prevEl: '#swiper-button-prev-rooms-offers',
+            }
+        
+        });
+    } else {
+        swipper_rooms_offers.classList.remove("swiper");
+        swiper_wrapper_rooms_offers.classList.remove("swiper-wrapper");
     }
 
-});
+}
+
+if(swipper_rooms_offers != null){
+    initSwiperRoomsOffers();
+}
 
 window.addEventListener('resize', () => {
+    /* TODO HACER LO DEL NULL */
     initSwiperRooms();
     initSwiperFacilities();
     initSwiperAbout();
     initSwiperCounter();
     initSwiperRoomsDetails();
+    initSwiperRoomsOffers();
 })
 
 
