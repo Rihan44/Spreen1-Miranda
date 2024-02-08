e5bcde671312127edd43f99de844e6db16361502
Rihan44 <asmuela_bustamante@hotmail.es>
2024-02-08 19:08:31 +0100

    Change: cambiado el aspecto visual de offers

--- scripts/offers.js
+++ scripts/offers.js
@@ -1,6 +1,7 @@
 
 let swipper_rooms_offers = document.querySelector('#swipper-rooms-offers');
 let swiper_wrapper_rooms_offers = document.querySelector('.swiper-wrapper__rooms-offers');
+let btn_book = document.getElementsByClassName('luxury-rooms-offers-container__button');
 
 function initSwiperRoomsOffers() {
     if (window.matchMedia("(max-width: 1000px").matches) {
@@ -30,3 +31,9 @@ window.addEventListener('resize', () => {
     initSwiperRoomsOffers();
 })
 
+for (let index = 0; index < btn_book.length; index++) {
+    btn_book[index].addEventListener('click', () => {
+        window.location.href = '../pages/rooms.html';
+    });
+    
+}
